04af2b258da6b61921e10b306a4ab2535527029f
kanchuts <wahono.gusty12@gmail.com>
2022-01-19 22:19:50 +0700

    delete preloader

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -22,12 +22,12 @@
 
 
 	<!-- preloader area start -->
-	<div class="preloader" id="preloader">
+	{{-- <div class="preloader" id="preloader">
 		<div class="loader loader-1">
 			<div class="loader-outter"></div>
 			<div class="loader-inner"></div>
 		</div>
-	</div>
+	</div> --}}
 	<!-- preloader area end -->
 
 	<!-- Header Area Start  -->
