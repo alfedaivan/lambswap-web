a7859bb83fcbb23628a118c0396c9b70dd1a0423
alfeda ivan widiaputra <alfedaivan@gmail.com>
2022-01-21 22:54:48 +0700

    fixing

--- resources/views/user/layouts/partner.blade.php
+++ resources/views/user/layouts/partner.blade.php
@@ -128,104 +128,45 @@
 			<div class="row">
                 <div class="col-md-2 col-6 mb-4">
                     <div class="partners">
-                        <a href="https://ethereum.org/en/"><img src="{{url('assets/images/aterium2.png')}}" alt=""></a>
+                        <a href="https://www.9capital.org"><img src="{{url('assets/images/9 Capital.png')}}" alt=""></a>
                     </div>
                 </div>
                 <div class="col-md-2 col-6 mb-4">
                     <div class="partners">
-                        <a href="https://www.binance.org/en/smartChain"><img src="{{url('assets/images/BNC.png')}}" alt=""></a>
+                        <a href="https://www.dcp.capital/"><img src="{{url('assets/images/Dragonfly Capital.png')}}" alt=""></a>
                     </div>
                 </div>
                 <div class="col-md-2 col-6 mb-4">
                     <div class="partners">
-                    <a href="https://solana.com/"><img src="{{url('assets/images/Solana.png')}}" alt=""></a>
+                    <a href="https://www.parafi.capital/"><img src="{{url('assets/images/Parafin Capital.png')}}" alt=""></a>
                     </div>
                 </div>
                 <div class="col-md-2 col-6 mb-4">
                     <div class="partners">
-                        <a href="https://polygon.technology/#"><img src="{{url('assets/images/Polugon.png')}}" alt=""></a>
+                        <a href="https://polygon.technology/#"><img src="{{url('assets/images/Spartan.png')}}" alt=""></a>
                     </div>
                 </div>
                 <div class="col-md-2 col-6 mb-4">
                     <div class="partners">
-                    <a href="https://fantom.foundation/"><img src="{{url('assets/images/Fantom.png')}}" alt=""></a>
+                    <a href="https://spartancapital.com/"><img src="{{url('assets/images/Spartan.png')}}" alt=""></a>
                     </div>
                 </div>
                 <div class="col-md-2 col-6 mb-4">
                     <div class="partners">
-                    <a href="https://www.avax.network/"><img src="{{url('assets/images/Avalanche.png')}}" alt=""></a>
+                    <a href="https://moonwhale.ventures/"><img src="{{url('assets/images/MoonWhale.png')}}" alt=""></a>
                     </div>
                 </div>
                 <div class="col-md-2 col-6 mb-4">
                     <div class="partners">
-                    <a href="https://www.okx.com/oec"><img src="{{url('assets/images/OKEX.png')}}" alt=""></a>
+                    <a href="https://www.blackventures.co/"><img src="{{url('assets/images/Black Ventures.png')}}" alt=""></a>
                     </div>
                 </div>
                 <div class="col-md-2 col-6 mb-4 ">
                     <div class="partners">
-                    <a href="https://arbitrum.io/"><img src="{{url('assets/images/Arbitrum.png')}}" alt=""></a>
+                    <a href="https://mythicalgames.com/"><img src="{{url('assets/images/Mythical.png')}}" alt=""></a>
                     </div>
                 </div>
-                <div class="col-md-2 col-6 mb-4 ">
-                    <div class="partners">
-                    <a href="https://www.optimism.io/"><img src="{{url('assets/images/OPTIMISM.png')}}" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-md-2 col-6 mb-4">
-                    <div class="partners">
-                    <a href="https://trustwallet.com/"><img src="{{url('assets/images/Trust.png')}}" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-md-2 col-6 mb-4">
-                    <div class="partners">
-                    <a href="https://metamask.io/"><img src="{{url('assets/images/Metamask.png')}}" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-md-2 col-6 mb-4 ">
-                    <div class="partners">
-                    <a href="https://coinmarketcap.com/"><img src="{{url('assets/images/CoinMarket.png')}}" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-md-2 col-6 mb-4">
-                    <div class="partners">
-                    <a href="https://www.coingecko.com/en"><img src="{{url('assets/images/CoinGecko.png')}}" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-md-2 col-6 mb-4">
-                    <div class="partners">
-                    <a href="https://mathwallet.org/en-us/"><img src="{{url('assets/images/MathWallet.png')}}" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-md-2 col-6 mb-4 ">
-                    <div class="partners">
-                    <a href="https://www.tokenpocket.pro/"><img src="{{url('assets/images/TokenPocket.png')}}" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-md-2 col-6 mb-4">
-                    <div class="partners">
-                    <a href="https://coin98.com/"><img src="{{url('assets/images/Coin98.png')}}" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-md-2 col-6 mb-4 ">
-                    <div class="partners">
-                    <a href="https://m.safepal.io/"><img src="{{url('assets/images/SafePal.png')}}" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-md-2 col-6 mb-4">
-                    <div class="partners">
-                    <a href="https://www.sollet.io/"><img src="{{url('assets/images/Sollet.png')}}" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-md-2 col-6 mb-4 ">
-                    <div class="partners">
-                    <a href="https://phantom.app/"><img src="{{url('assets/images/Phantom.png')}}" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-md-2 col-6 mb-4 ">
-                    <div class="partners">
-                    <a href="https://walletconnect.com/"><img src="{{url('assets/images/Connect.png')}}" alt=""></a>
-                    </div>
-                </div>
+
 			</div>
 		</div>
 	</section>
