f09f1e9e006d56dd5dba14c7e19fe4745c861756
kanchuts <wahono.gusty12@gmail.com>
2022-01-22 03:36:12 +0700

    mail

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>websitepercobaan.com</title>
+    <title>{{ $details['title'] }}</title>
 </head>
 <body>
     <h1>{{ $details['title'] }}</h1>
-    <p>{{ $details['body'] }}</p>
-
+    <p>Name: {{ $details['name'] }}</p>
+    <p>Wallet Address: {{$details['wallet_address']}}</p>
+    <p>Username Telegram: {{ $details['username_telegram'] }}</p>
+    <p>Amount BUSD: {{ $details['amount_busd'] }}</p>
+    <p>Amount LST: {{ $details['amount_lst'] }}</p>
     <p>Thank you</p>
 </body>
 </html>
