0e9ef270eb9598f6f7fec68f7e98b2ea1fafd4d3
kanchuts <wahono.gusty12@gmail.com>
2022-01-28 23:30:54 +0700

    fixing meta head

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	{{-- <meta http-equiv="X-UA-Compatible" content="IE=edge" /> --}}
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<meta name="forntEnd-Developer" content="Mamunur Rashid">
+    <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
