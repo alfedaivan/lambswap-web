c4fc5b27f04ca3c7b6ae51869af48ac96ccbac74
kanchuts <wahono.gusty12@gmail.com>
2022-02-17 01:01:00 +0700

    fixing rss2json

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,6 +17,7 @@ class UserController extends Controller
         $onGoing  = Ido::where('status', 'On Going')->first();
         $url = 'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/@lambswaptoken/feed';
         $response = $client->request('GET', $url, [
+            'api_key' => 'kx4rwrecim6znwtbgky8iffinxuna5qj7nggg7zz',
             'verify'  => false,
         ]);
 
